7d6405d68b75a084c82010ed3f87091896e21876
Argenthands <alejandro.david.benolol@gmail.com>
2022-01-17 20:36:39 +0200

    Boilerplate Done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+import { Route, Routes } from 'react-router-dom'
 import './App.css';
+import Header from './pages/Header/Header';
+import Body from './pages/Body/Body';
+import Footer from './pages/Footer/Footer';
 
+/*
+<Route path='' element={<></>}/>
+*/
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path='/' element={
+          <>
+            <Header />
+            <Body />
+            <Footer />
+          </>
+        }/>
+      </Routes>
+      <h1>Let's Cook!</h1>
     </div>
   );
 }
